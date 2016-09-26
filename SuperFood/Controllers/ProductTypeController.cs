343816694a84bbe2075add9dae4aec92156da4a3
Katarina1330 <dordevic.katarina@outlook.com>
2016-09-25 19:29:28 -0700

    Added ProductType Administration

--- SuperFood/Controllers/ProductTypeController.cs
+++ SuperFood/Controllers/ProductTypeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SuperFood.Models;
+using SuperFood.Shared.Data.Implementations;
+using SuperFood.Shared.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,39 @@ namespace SuperFood.Controllers
 {
     public class ProductTypeController : Controller
     {
+        private readonly IRepositoryService _repository;
+
+        public ProductTypeController(IRepositoryService repository)
+        {
+            _repository = repository;
+        }
+
         // GET: ProductType
         public ActionResult Index()
         {
+            var productTypes = _repository.Read<ProductType>()
+                .Select(p => new ProductTypeViewModels
+                {
+                    Id = p.Id, 
+                    Name = p.Name
+                }).ToList(); 
+
+            return View(productTypes);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductTypeViewModels productTypeViewModel)
+        {
+            var productType = new ProductType();
+            productType.Name = productTypeViewModel.Name;
+            _repository.Create<ProductType>(productType);
+            ModelState.Clear();
+
             return View();
         }
     }
--- SuperFood/Models/ProductTypeViewModels.cs
+++ SuperFood/Models/ProductTypeViewModels.cs
@@ -7,5 +7,7 @@ namespace SuperFood.Models
 {
     public class ProductTypeViewModels
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
