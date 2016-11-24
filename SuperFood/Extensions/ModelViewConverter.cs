776ee54370ea819fe0802af9df6d2024372a60e0
Katarina1330 <dordevic.katarina@outlook.com>
2016-11-23 23:53:12 -0800

    Added Extensions classes

---
+++ SuperFood/Extensions/ModelViewConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperFood.Extensions
+{
+    public static class ModelViewConverter
+    {
+    }
+}
